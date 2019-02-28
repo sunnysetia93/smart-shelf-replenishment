3745f946ceaf64597129e79ca0027027419d5953
sunny setia <sunnysetia93@gmail.com>
2019-02-28 17:14:30 +0530

    opencv updated with new webcam

--- opencv-py-module/main1.py
+++ opencv-py-module/main1.py
@@ -1,12 +1,19 @@
 import base64
 import cv2
 import zmq
+import requests
+import json
 
 context = zmq.Context()
 publisher = context.socket(zmq.PUB)
 publisher.bind("tcp://127.0.0.1:9001")
 
-camera = cv2.VideoCapture(1)  # init the camera
+def sendFrames(buffer):
+    jpg_as_text = base64.b64encode(buffer)
+    publisher.send(jpg_as_text)
+    return
+
+camera = cv2.VideoCapture(1)
 # camera.set(3,160)
 # camera.set(4,120)
 
@@ -15,8 +22,7 @@ while True:
         grabbed, frame = camera.read()  # grab the current frame
         frame = cv2.resize(frame, (640, 480))  # resize the frame
         encoded, buffer = cv2.imencode('.jpg', frame)
-        jpg_as_text = base64.b64encode(buffer)
-        publisher.send(jpg_as_text)
+        sendFrames(buffer)
     except KeyboardInterrupt:
         camera.release()
         cv2.destroyAllWindows()
